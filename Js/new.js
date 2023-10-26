3891ae540afc6129d6d4a82b8dfde5eff7ad825c
arhaamwanii <arhamwani765@gmail.com>
2023-10-26 20:33:42 +0530

    .

---
+++ Js/new.js
@@ -0,0 +1,13 @@
+let a = 5;
+let b = 10;
+let temp;
+
+// Using a temporary variable to swap the values
+temp = a;
+a = b;
+b = temp;
+
+console.log( a); // Output: 10
+console.log( b); // Output: 5
+
+
