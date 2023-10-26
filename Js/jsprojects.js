53a6ee120803f01c50cd0d267a330cb6f88a11af
arhaamwanii <arhamwani765@gmail.com>
2023-10-27 02:27:50 +0530

    .

--- Js/jsprojects.js
+++ Js/jsprojects.js
@@ -1,21 +1,10 @@
-function getMilk(bottle) {   
-    console.log("leaveHouse");
-    console.log("moveRight");
-    console.log("moveRight");
-    console.log("moveUp");
-    console.log("moveUp");
-    console.log("moveUp");
-    console.log("moveUp");
-    console.log("moveRight" + bottle + "you cant do shit");
-    console.log("moveRight");
-    console.log("moveLeft");
-    console.log("moveLeft");
-    console.log("moveDown");
-    console.log("moveDown");
-    console.log("moveDown");
-    console.log("moveDown");
-    console.log("moveLeft");
-    console.log("moveLeft");
-    console.log("enterHouse");
-  }
+
+var x = prompt("Number of bottles you want to buy");
+
+function cost (numberBottles){
+    var cost = numberBottles * 4.99 ;
+    alert(Math.floor(cost) + " it will cost this much to buy " + numberBottles + " bottles")
+}
+
+cost(x);23
 
