8251e8f872a4c03f24f5e229e3485f0259cbd5e2
arhaamwanii <arhamwani765@gmail.com>
2023-10-27 19:56:01 +0530

    .

--- Js/Intermediate/try.js
+++ Js/Intermediate/try.js
@@ -13,7 +13,8 @@
 // }
 
 // var randomNumber = getRandomNumber(1, 100); // Generates a random number between 1 and 10
-// let y =  (console.log(Math.round(randomNumber)));
+// let y =  ((Math.round(randomNumber)));
+// console.log(y)
 
 // if (y < 10){
 //     console.log("less than 10");
@@ -21,23 +22,87 @@
 //     console.log("it was awfull");
 // }
 
-prompt("")
 
 
-function checkNumber(number) {
-    if (number > 10) {
-        return "Greater than 10";
-    } else if (number < 10) {
-        return "Less than 10";
-    } else {
-        return "Equal to 10";
+// function checkNumber(number) {
+//     if (number > 10) {
+//         return "Greater than 10";
+//     } else if (number < 10) {
+//         return "Less than 10";
+//     } else {
+//         return "Equal to 10";
+//     }
+// }
+
+// console.log(yes)
+// // Example usage:
+// console.log(checkNumber(firstno));  // Output: Less than 10
+// console.log(checkNumber(secondno)); // Output: Equal to 10
+// console.log(checkNumber(thirdno)); // Output: Greater than 10
+
+// newFunction();
+
+
+// function newFunction() {
+//     var x = prompt("size");
+
+//     if (x == 12) {
+//         console.log("go ahead");
+//     }
+//     else if (x > 12) {
+//         console.log("thats too big");
+//     }
+//     else if (x < 12) {
+//         console.log("thats small");
+//     }
+//     else if (x === 13 || x === 11) {
+//         console.log("there is a chance");
+//     }
+// }
+
+// function newFunction() {
+//     var x = prompt("size");
+
+//     if (x == 12) {
+//         console.log("go ahead");
+//     }
+//     else if (x > 12) {
+//         console.log("that's too big");
+//     }
+//     else if (x < 12) {
+//         console.log("that's small");
+//     }
+//     else if (x === 13 || x === 11) {
+//         console.log("there is a chance");
+//     }
+//     else {
+//         console.log("invalid input");
+//     }
+// }
+
+
+
+function newFunction() {
+    var x = prompt("size");
+    
+    if (x == 13 || x == 11) {
+        console.log("there is a chance");
+    }
+    else if (x == 12) {
+        console.log("correct size");
+    }
+    else if (x > 12) {
+        console.log("that's too big");
+    }
+    else if (x < 12) {
+        console.log("that's small");
+    }
+    else {
+        console.log("invalid input");
     }
 }
 
-// Example usage:
-console.log(checkNumber(firstno));  // Output: Less than 10
-console.log(checkNumber(secondno)); // Output: Equal to 10
-console.log(checkNumber(thirdno)); // Output: Greater than 10
+newFunction(); // Call the function
 
 
 
