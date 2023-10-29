d1094712501fa2b85a9f3189a76694f5a105c982
arhaamwanii <arhamwani765@gmail.com>
2023-10-30 01:35:23 +0530

    .

--- Js/Intermediate/trialcode.js
+++ Js/Intermediate/trialcode.js
@@ -329,6 +329,17 @@
 // document.getElementById("head").classList.toggle("remove");
 
 
+function add(num1 , num2){
+    return num1 + num2;
+}
+
+function multiply(num1, num2){
+    return num1 * num2;
+}
+
+function calculator(num1 , num2 , operator){
+    return operator(num1 , num2);
+}
 
 
 
