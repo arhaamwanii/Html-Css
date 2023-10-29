abe5cdfede77a9e4302a38094b49ecd540bff53b
arhaamwanii <arhamwani765@gmail.com>
2023-10-30 00:03:22 +0530

    .

--- Js/Intermediate/Drum Kit Starting Files/index.js
+++ Js/Intermediate/Drum Kit Starting Files/index.js
@@ -1,4 +1,4 @@
-document.querySelector("button").addEventListener("click", handleClick)
+// document.querySelector("button").addEventListener("click", handleClick)
 // selecting the location ,, type ,, listner ,, 
 // this creates a listner to the buttona and calls to do something when the button is clicked
 // it calls the function
@@ -6,19 +6,24 @@ document.querySelector("button").addEventListener("click", handleClick)
 //adding the parenthese at the end will make it get called as soon as the eventlistner is added it does not wait for things to be clicked 
 
 
+// it will loop througn all buttons
+
+// function handleClick(){
+//     alert("I got CicckdCLicked")
+
+
+
+//  
+
 var numberOfDrumButtons = document.querySelectorAll(".drum").length;
 
-for (var i = 0; i< numberOfDrumButtons ; i++)
+for (var i = 0; i < numberOfDrumButtons ; i++)
+{
 
-
-document.querySelector("button")[].addEventListener("click", function(){
-    alert("i got clicked")
+document.querySelectorAll(".drum")[i].addEventListener("click", function(){
+    alert("i got clicked");
 })
 
-//it will loop througn all buttons
-
-function handleClick(){
-    alert("I got CicckdCLicked")
 };
 
-//
+
