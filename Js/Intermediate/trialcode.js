5f04f19767123372fcc1874e31ff1839711f1cc0
arhaamwanii <arhamwani765@gmail.com>
2023-10-30 03:41:24 +0530

    .

--- Js/Intermediate/trialcode.js
+++ Js/Intermediate/trialcode.js
@@ -337,9 +337,15 @@ function multiply(num1, num2){
     return num1 * num2;
 }
 
-function calculator(num1 , num2 , operator){
-    return operator(num1 , num2);
+function divide(num1 , num2){
+    return num1/num2;
 }
+
+function calculator(num1 , num2 , sex){
+    return sex(num1 , num2);
+}
+
+
 
 
 
