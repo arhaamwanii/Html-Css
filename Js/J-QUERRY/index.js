95f148ab9f7a35f22f7aa2a35162de4937145be7
arhaamwanii <arhamwani765@gmail.com>
2023-10-31 03:54:44 +0530

    .

--- Js/J-QUERRY/index.js
+++ Js/J-QUERRY/index.js
@@ -1,3 +1,13 @@
-$("h1").css("color" , "red");
+//ADDING CSS
+
+$("h1").css({
+    "font-size": "3rem",
+    "color": "red",
+})
+
 
 $("button").css("background-color" , "green");
+
+//ADDING CSS
+
+$("h1").addClass("boom")
