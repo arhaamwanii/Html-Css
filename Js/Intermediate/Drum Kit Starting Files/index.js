16d22552b5c5f36a1dae04b0e9abd6306e665428
arhaamwanii <arhamwani765@gmail.com>
2023-10-30 23:22:28 +0530

    .

--- Js/Intermediate/Drum Kit Starting Files/index.js
+++ Js/Intermediate/Drum Kit Starting Files/index.js
@@ -159,3 +159,77 @@ function showAlert(){
 //this new here makes a new input , object using the factory
 
 
+
+
+var numberOfBtns = document.getElementsByClassName("drum").length ;
+
+
+for( i = 0 ; i< numberOfBtns ; i++){
+
+document.getElementsByClassName("drum")[i].addEventListener("click" , showAlert);
+
+function showAlert(){
+    
+
+    var buttonInnerHTML = this.innerHTML;
+    
+    switch (buttonInnerHTML) {
+        case "w":
+            
+        var audio1 = new Audio('sounds/crash.mp3')
+        audio1.play();
+
+        break;
+        
+        case "a":
+        var audio2 = new Audio('sounds/kick-bass.mp3')
+        audio2.play();
+
+        break;
+
+        case"s":
+        var audio3 = new Audio("sounds/snare.mp3")
+        audio3.play();
+        
+        break;
+
+        case"d":
+        var audio4 = new Audio("sounds/tom-1.mp3")
+        audio4.play();
+
+        break;
+
+        case"j":
+        var audio5 = new Audio("sounds/tom-2.mp3")
+        audio5.play();
+
+        break;
+
+        case"k":
+        var audio6 = new Audio("sounds/tom-3.mp3")
+        audio6.play();
+
+        break;
+
+        case"l":
+        var audio7 = new Audio("sounds/tom-4.mp3")
+        audio7.play();
+        break;
+
+        default:
+            console.log(buttonInnerHTML);
+            break;
+
+    }
+}
+}
+
+
+// document.addEventListenet('keydown', function(ok){
+//     alert('lifes Good');
+// }) ;
+
+document.addEventListener('keydown', function(ok) {
+    alert('ok');
+});
+
