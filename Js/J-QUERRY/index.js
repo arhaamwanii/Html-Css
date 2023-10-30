3237dcf3451ac170c285985f2491bc81ba947ebd
arhaamwanii <arhamwani765@gmail.com>
2023-10-31 04:24:26 +0530

    .

--- Js/J-QUERRY/index.js
+++ Js/J-QUERRY/index.js
@@ -4,10 +4,25 @@ $("h1").css({
     "font-size": "3rem",
     "color": "red",
 })
-
-
 $("button").css("background-color" , "green");
 
 //ADDING CSS
 
 $("h1").addClass("boom")
+
+//checkign calsses   - gives back a boolein
+
+var check = $("h1").hasClass("sex");
+console.log(check);
+
+
+//adding text
+
+$( ".one").text("how you doing brotheer")
+
+//adding innerHTML
+
+$("button").html("<em> how are you today</em>")
+
+
+$( ".one").text("how you doing brotheer")
